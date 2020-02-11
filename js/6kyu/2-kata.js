ac8d9ec2a3c7a451f9d3be5fbf5b5aee5f2881e4
iFrosta <samgolovchenko@gmail.com>
2020-02-11 22:24:00 +0300

    6kyu - 2-kata - compete

--- js/6kyu/2-kata.js
+++ js/6kyu/2-kata.js
@@ -1,5 +1,9 @@
-const digital_root = n => {
-  return n.toString().length === 1 ? n : digital_root(Number(n).toString().split('').reduce((acc,item) => Number(acc) + Number(item), 0))
+// const digital_root = n => {
+//   return n.toString().length === 1 ? n : digital_root(Number(n).toString().split('').reduce((acc,item) => Number(acc) + Number(item), 0))
+// }
+
+function digital_root(n) {
+  return (n - 1) % 9 + 1;
 }
 
 console.log(digital_root(16))// 7
