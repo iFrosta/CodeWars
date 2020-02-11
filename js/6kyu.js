34c41184d822fa30265958c60d17a4c5e9ec1846
iFrosta <samgolovchenko@gmail.com>
2020-02-11 21:35:33 +0300

    6kyu - draft

---
+++ js/6kyu.js
@@ -0,0 +1,20 @@
+function arrayDiff(a, b) {
+  if (b.length !== 0) {
+    let filtered = a;
+    for (let i = 0; i < b.length; i++) {
+      filtered = filtered.filter(item => item !== b[i])
+    }
+    return filtered
+  } else {
+    return a
+  }
+}
+
+console.log(arrayDiff([], [4, 5])) // []
+console.log(arrayDiff([3, 4], [3])) // [4]
+console.log(arrayDiff([1, 8, 2], [])) // [1,8,2]
+
+console.log(arrayDiff([1, 2, 2, 2, 3], [2])) // [1,3]
+console.log(arrayDiff([1, 2], [1])) // [2]
+
+console.log(arrayDiff([13, -7, -13, -20, 4, -5, 10, -11, 0, -13, 14], [-20, 10, 14, -13, 0, -5, 4])) // [13, -7, -11]
