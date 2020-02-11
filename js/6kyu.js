27f17dddc437d8582d44e3e1eae6ea15323ab405
iFrosta <samgolovchenko@gmail.com>
2020-02-11 21:47:22 +0300

    6kyu - complete

--- js/6kyu.js
+++ js/6kyu.js
@@ -1,11 +1,13 @@
 function arrayDiff(a, b) {
-  if (b.length) {
-    for (let i = 0; i < b.length; i++) {
-      a = a.filter(item => item !== b[i])
-    }
-  }
-  return a
+  return a.filter(e => !b.includes(e));
 }
+
+// Description:
+// Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+// It should remove all values from list a, which are present in list b.
+//   arrayDiff([1,2],[1]) == [2]
+// If a value is present in b, all of its occurrences must be removed from the other:
+//   arrayDiff([1,2,2,2,3],[2]) == [1,3]
 
 console.log(arrayDiff([], [4, 5])) // []
 console.log(arrayDiff([3, 4], [3])) // [4]
