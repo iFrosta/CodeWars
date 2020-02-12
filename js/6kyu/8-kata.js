12277298d5012ea4071b9efdd8d161f2bd7c957c
iFrosta <samgolovchenko@gmail.com>
2020-02-12 16:59:44 +0300

    6kyu - 8-kata - complete

--- js/6kyu/8-kata.js
+++ js/6kyu/8-kata.js
@@ -1,4 +1,4 @@
-const songDecoder = song => song.replace(/(WUB)+/gi, ' ').trim()
+const songDecoder = song => song.replace(/(WUB)+/g, ' ').trim()
 
 
 //Polycarpus works as a DJ in the best Berland nightclub, and he often uses dubstep music in his performance. Recently, he has decided to take a couple of old songs and make dubstep remixes from them.
