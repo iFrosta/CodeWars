3ac0d62d524edeaadcb1a229f1b8d0a8959aefc0
iFrosta <samgolovchenko@gmail.com>
2020-02-12 12:29:10 +0300

    6kyu - 4-kata - draft

---
+++ js/6kyu/4-kata.js
@@ -0,0 +1,12 @@
+function iqTest(numbers) {
+  let counts = {odd: [], even: []}
+  numbers.match(/\d+/g).map(Number).forEach(function (el, idx) {
+    (el % 2 === 0) ? counts.odd.push(`${idx + 1}`) : counts.even.push(`${idx + 1}`)
+  })
+  return parseInt((counts.odd.length === 1) ? counts.odd : counts.even, 10)
+}
+
+
+console.log(iqTest("2 4 7 8 10")) // 3 - Third number is odd, while the rest of the numbers are even
+console.log(iqTest("1 2 1 1")) // 2 // Second number is even, while the rest of the numbers are odd
+console.log(iqTest("1 2 2")) // 1
