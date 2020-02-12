6c16628866c958ad17ed548b17897f56a3867fc3
iFrosta <samgolovchenko@gmail.com>
2020-02-12 18:50:40 +0300

    6kyu - 10-kata - complete

--- js/6kyu/10-kata.js
+++ js/6kyu/10-kata.js
@@ -1,18 +1,23 @@
-function high(x) {
-  let alp = 'abcdefghijklmnopqrstuvwxyz', words = x.toLowerCase().match(/([^\s]+)/g), output = {}, word = {key: '', value: 0}
-  words.forEach(el => {
-    output[el] = []
-    el.toLowerCase().match(/[a-z]/g).forEach(a => output[el].push(alp.indexOf(a)))
-    output[el] = output[el].reduce((a, b) => a + b)
-  })
-  for (const [key, value] of Object.entries(output)) {
-    if (value > word.value) {
-      word.value = value
-      word.key = key
-    }
-  }
-  return word.key
+function high(x){
+  let xo = x.split(' ').map(x => [...x].reduce((a, b) => a+b.charCodeAt(0) - 96, 0))
+  return x.split(' ')[xo.indexOf(Math.max(...xo))];
 }
+
+// function high(x) {
+//   let alp = 'abcdefghijklmnopqrstuvwxyz', words = x.toLowerCase().match(/([^\s]+)/g), output = {}, word = {key: '', value: 0}
+//   words.forEach(el => {
+//     output[el] = []
+//     el.toLowerCase().match(/[a-z]/g).forEach(a => output[el].push(alp.indexOf(a)))
+//     output[el] = output[el].reduce((a, b) => a + b)
+//   })
+//   for (const [key, value] of Object.entries(output)) {
+//     if (value > word.value) {
+//       word.value = value
+//       word.key = key
+//     }
+//   }
+//   return word.key
+// }
 
 
 console.log(high('man i need a taxi up to ubud')) // 'taxi'
