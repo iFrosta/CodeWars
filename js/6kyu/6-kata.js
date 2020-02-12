ecbb587540fa64bf1b73adfd6d14b1ec1b432938
iFrosta <samgolovchenko@gmail.com>
2020-02-12 15:40:59 +0300

    6kyu - 6-kata - complete

--- js/6kyu/6-kata.js
+++ js/6kyu/6-kata.js
@@ -1,12 +1,27 @@
 function narcissistic(value) {
-  let _value = value.toString().split('').map(Number)
-  if (_value) {
-    let output = []
-    _value.forEach(el => output.push(Math.pow(el, _value.length)))
-    return (output.reduce((a, b) => a + b, 0) === value)
-  } else return false
+  let _value = value.toString().split(''), output = []
+
+  _value.forEach(el => output.push(Math.pow(el, _value.length)))
+  return (output.reduce((a, b) => a + b, 0) === value)
 }
 
+// function narcissistic( value ) {
+//   return ('' + value).split('').reduce(function(p, c){
+//     return p + Math.pow(c, ('' + value).length)
+//   }, 0) == value;
+// }
+
+
+//A Narcissistic Number is a number which is the sum of its own digits, each raised to the power of the number of
+// digits in a given base. In this Kata, we will restrict ourselves to decimal (base 10).
+// For example, take 153 (3 digits):
+//     1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
+// and 1634 (4 digits):
+//     1^4 + 6^4 + 3^4 + 4^4 = 1 + 1296 + 81 + 256 = 1634
+// The Challenge:
+// Your code must return true or false depending upon whether the given number is a Narcissistic number in base 10.
+// Error checking for text strings or other invalid inputs is not required,
+// only valid integers will be passed into the function.
 
 console.log(narcissistic(7)) // true, "7 is narcissistic"
 console.log(narcissistic(371)) // true, "371 is narcissistic"
