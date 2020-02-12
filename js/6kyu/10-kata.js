5e10957d17f819e2702e3f0819ac42c39bcbdd48
iFrosta <samgolovchenko@gmail.com>
2020-02-12 18:46:58 +0300

    6kyu - 10-kata - draft

---
+++ js/6kyu/10-kata.js
@@ -0,0 +1,20 @@
+function high(x) {
+  let alp = 'abcdefghijklmnopqrstuvwxyz', words = x.toLowerCase().match(/([^\s]+)/g), output = {}, word = {key: '', value: 0}
+  words.forEach(el => {
+    output[el] = []
+    el.toLowerCase().match(/[a-z]/g).forEach(a => output[el].push(alp.indexOf(a)))
+    output[el] = output[el].reduce((a, b) => a + b)
+  })
+  for (const [key, value] of Object.entries(output)) {
+    if (value > word.value) {
+      word.value = value
+      word.key = key
+    }
+  }
+  return word.key
+}
+
+
+console.log(high('man i need a taxi up to ubud')) // 'taxi'
+console.log(high('what time are we climbing up the volcano')) //  volcano'
+console.log(high('take me to semynak')) // 'semynak'
