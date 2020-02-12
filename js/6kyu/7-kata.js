788dbe141db66d5b84f30317d14dbd3cdcbddf88
iFrosta <samgolovchenko@gmail.com>
2020-02-12 16:10:17 +0300

    6kyu - 7-kata - complete

--- js/6kyu/7-kata.js
+++ js/6kyu/7-kata.js
@@ -1,4 +1,4 @@
-const isPangram = string => (string.toLowerCase().match(/[A-Za-z]/g).sort().filter((x, i, a) => !i || x != a[i - 1]).length === 26)
+const isPangram = string => string.toLowerCase().match(/[a-z]/g).sort().filter((x, i, a) => !i || x != a[i - 1]).length === 26
 
 
 console.log(isPangram("The quick brown fox jumps over the lazy dog.")) // true
