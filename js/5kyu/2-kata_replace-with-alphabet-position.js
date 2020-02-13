a176afbbb3facbc357e290050a02bdd2e1763d1b
iFrosta <samgolovchenko@gmail.com>
2020-02-13 10:25:08 +0300

    5kyu - 2-kata - complete

---
+++ js/5kyu/2-kata_replace-with-alphabet-position.js
@@ -0,0 +1,21 @@
+const alphabetPosition = text => (text = text.toLowerCase().match(/[a-z]/g)) ? text.map(x => x.charCodeAt(0) - 96).join(' ') : ''
+
+//function alphabetPosition(text) {
+//   return text
+//     .toUpperCase()
+//     .match(/[a-z]/gi)
+//     .map( (c) => c.charCodeAt() - 64)
+//     .join(' ');
+// }
+
+//Description:
+// In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+// If anything in the text isn't a letter, ignore it and don't return it.
+// "a" = 1, "b" = 2, etc.
+// Example
+// alphabetPosition("The sunset sets at twelve o' clock.")
+// Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" (as a string)
+
+console.log(alphabetPosition("The sunset sets at twelve o' clock.")) // "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
+console.log(alphabetPosition("The narwhal bacons at midnight.")) // "20 8 5 14 1 18 23 8 1 12 2 1 3 15 14 19 1 20 13 9 4 14 9 7 8 20"
+console.log(alphabetPosition(":/4]||}+")) // ""
