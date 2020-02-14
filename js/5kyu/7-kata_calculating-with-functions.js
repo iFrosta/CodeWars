b879c5553705625fb065a52de70e9eaccc9966b4
iFrosta <samgolovchenko@gmail.com>
2020-02-14 23:48:11 +0300

    5kyu - 7-kata - complete

---
+++ js/5kyu/7-kata_calculating-with-functions.js
@@ -0,0 +1,26 @@
+function expression(number, operation){
+  if(!operation) return number
+  return operation(number)
+}
+
+function zero(operation) {return expression(0, operation)}
+function one(operation) {return expression(1, operation)}
+function two(operation) {return expression(2, operation)}
+function three(operation) {return expression(3, operation)}
+function four(operation) {return expression(4, operation)}
+function five(operation) {return expression(5, operation)}
+function six(operation) {return expression(6, operation)}
+function seven(operation) {return expression(7, operation)}
+function eight(operation) {return expression(8, operation)}
+function nine(operation) {return expression(9, operation)}
+
+function plus(x) {return function(y) {return y + x}}
+function minus(x) {return function(y) {return y - x}}
+function times(x) {return function(y) {return Math.floor( y * x)}}
+function dividedBy(x) {return function(y) {return Math.floor(y / x)}}
+
+console.log(zero(times(five()))) // 35
+console.log(seven(times(five()))) // 35
+console.log(four(plus(nine()))) // 13
+console.log(eight(minus(three()))) // 5
+console.log(six(dividedBy(two()))) // 3
