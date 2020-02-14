5f6051d887a554105488911a1f9e7d1b64d235b0
iFrosta <samgolovchenko@gmail.com>
2020-02-14 22:05:47 +0300

    5kyu - 5-kata - complete

--- js/5kyu/5-kata_equal_sides_of_an_array.js
+++ js/5kyu/5-kata_equal_sides_of_an_array.js
@@ -1,42 +1,34 @@
-function findEvenIndex(arr) {
-  let idx
-  for (let y = 0; y < arr.length; y++) {
-    let left = 0, right = 0
-    for (let x = 0; x < y; x++) {
-      left += arr[x]
-    }
-    for (let x = arr.length - 1; x > y; x--) {
-      right += arr[x]
-    }
-    if (left === right) {
-      idx = y
-      break
-    }
-  }
-  return (idx) ? idx : -1;
-}
+const sum = (a, from, to) => a.slice(from, to).reduce((a, b) => a + b, 0)
+const findEvenIndex = a => a.findIndex((el, i) => sum(a, 0, i) === sum(a, i + 1));
 
-// left = arr.reduce((a, b) => (a < index) ? a + b : 0)
+
+// left = arr.map((e, i) => i).filter(i => i <= y).map(i => arr[i]).reduce((a, b) => a + b)
+
+// left = arr.map((e, i) => e > 0 ? i : null).filter(i => i <= y).map(i => arr[i]).reduce((a, b) => a + b)
+// right = arr.map((el, index) => el > 0 ? index : null).filter(index => index > y).reduce((a, b) => a + b)
+//right = arr.map((e, i) => e > 0 ? i : null).filter(i => i > y).map(i => arr[i])//.reduce((a, b) => a + b)
+
+// left = arr.map((el, index) => el > 0 ? index : null).filter(index => index < y)
+// console.log(arr.map((el, index) => el > 0 ? index : null).filter(index => index <= y).reduce((a, b) => a + b))
 
 // function findEvenIndex(arr) {
 //   let idx
 //   for (let y = 0; y < arr.length; y++) {
 //     let left = 0, right = 0
-//     for (let x = 0; x < y; x++) {
-//       left += arr[x]
-//     }
-//     for (let x = arr.length - 1; x > y; x--) {
-//       right += arr[x]
-//     }
+//
+//     for (let x = 0; x < y; x++) left += arr[x]
+//     for (let x = arr.length - 1; x > y; x--) right += arr[x]
+//
 //     if (left === right) {
 //       idx = y
 //       break
 //     }
 //   }
-//   return (idx) ? idx : -1;
+//   return (idx === undefined) ? -1 : idx
 // }
 
 console.log(findEvenIndex([1, 2, 3, 4, 3, 2, 1])) // 3, "The array was: [1,2,3,4,3,2,1] \n"
 console.log(findEvenIndex([1, 100, 50, -51, 1, 1])) // , 1, "The array was: [1,100,50,-51,1,1] \n";
 console.log(findEvenIndex([1, 2, 3, 4, 5, 6])) // , -1, "The array was: [1,2,3,4,5,6] \n"
 console.log(findEvenIndex([20, 10, 30, 10, 10, 15, 35])) // , 3, "The array was: [20,10,30,10,10,15,35] \n"
+console.log(findEvenIndex([8, 0])) // 0
