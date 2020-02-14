70fe2fbdfbcb083cb0e6a63772bd8fb66e1b7513
iFrosta <samgolovchenko@gmail.com>
2020-02-14 23:49:44 +0300

    5kyu - 7-kata - complete

--- js/5kyu/7-kata_calculating-with-functions.js
+++ js/5kyu/7-kata_calculating-with-functions.js
@@ -1,23 +1,32 @@
-function expression(number, operation){
-  if(!operation) return number
-  return operation(number)
-}
+'zero one two three four five six seven eight nine'.split(' ').forEach(
+  (mth, num) => this[mth] = (f = val => val) => f(num)
+)
 
-function zero(operation) {return expression(0, operation)}
-function one(operation) {return expression(1, operation)}
-function two(operation) {return expression(2, operation)}
-function three(operation) {return expression(3, operation)}
-function four(operation) {return expression(4, operation)}
-function five(operation) {return expression(5, operation)}
-function six(operation) {return expression(6, operation)}
-function seven(operation) {return expression(7, operation)}
-function eight(operation) {return expression(8, operation)}
-function nine(operation) {return expression(9, operation)}
+let plus      = (r) => (l) => l + r
+let minus     = (r) => (l) => l - r
+let times     = (r) => (l) => l * r
+let dividedBy = (r) => (l) => l / r
 
-function plus(x) {return function(y) {return y + x}}
-function minus(x) {return function(y) {return y - x}}
-function times(x) {return function(y) {return Math.floor( y * x)}}
-function dividedBy(x) {return function(y) {return Math.floor(y / x)}}
+// function expression(number, operation){
+//   if(!operation) return number
+//   return operation(number)
+// }
+//
+// function zero(operation) {return expression(0, operation)}
+// function one(operation) {return expression(1, operation)}
+// function two(operation) {return expression(2, operation)}
+// function three(operation) {return expression(3, operation)}
+// function four(operation) {return expression(4, operation)}
+// function five(operation) {return expression(5, operation)}
+// function six(operation) {return expression(6, operation)}
+// function seven(operation) {return expression(7, operation)}
+// function eight(operation) {return expression(8, operation)}
+// function nine(operation) {return expression(9, operation)}
+//
+// function plus(x) {return function(y) {return y + x}}
+// function minus(x) {return function(y) {return y - x}}
+// function times(x) {return function(y) {return Math.floor( y * x)}}
+// function dividedBy(x) {return function(y) {return Math.floor(y / x)}}
 
 console.log(zero(times(five()))) // 35
 console.log(seven(times(five()))) // 35
