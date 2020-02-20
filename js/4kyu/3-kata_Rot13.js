805e99118a55702f80737cf9326a2c224a1ac2c2
iFrosta <samgolovchenko@gmail.com>
2020-02-20 20:08:12 +0300

    4kyu - 3-kata - complete

--- js/4kyu/3-kata_Rot13.js
+++ js/4kyu/3-kata_Rot13.js
@@ -1,11 +1,4 @@
-// function rot13(t) {
-//   let i = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz', o = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm'
-//   return t.split('').map(x => i.indexOf(x) > -1 ? o[i.indexOf(x)] : x).join('')
-// }
-
-// const rot13 = t => t.split('').map(x => 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.indexOf(x) > -1 ? 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm'['ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.indexOf(x)] : x).join('')
-
-const rot13 = str => str.replace(/[a-z]/gi, l => String.fromCharCode(l.charCodeAt(0) + (l.toLowerCase() <= 'm' ? 13: -13)))
+const rot13 = str => str.replace(/[a-z]/gi, l => String.fromCharCode(l.charCodeAt(0) + (l.toLowerCase() <= 'm' ? 13 : -13)))
 
 
 console.log(rot13("test")) // grfg
